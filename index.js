c9ab2531846f44c7a16631c17ab7763f92e8d42e
ComicIvans <83135925+ComicIvans@users.noreply.github.com>
2022-11-16 19:19:36 +0100

    Ajustes en la plantilla y números modificables

--- index.js
+++ index.js
@@ -1,10 +1,29 @@
-const puppeteer = require('puppeteer');
+import puppeteer from 'puppeteer';
+import { PDFDocument, StandardFonts, rgb } from 'pdf-lib';
+import fs from 'fs/promises';
+
+
+async function removeLastPage(path) {
+  var pdfData = await fs.readFile(path);
+  var pdfDoc = await PDFDocument.load(pdfData);
+  pdfDoc.removePage(1)
+  var pdfData = await pdfDoc.save();
+  await fs.writeFile(path, pdfData);
+}
 
 (async () => {
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
-    await page.goto('http://localhost:5500');
-    var cells = await page.$$('.cell');
-    await page.pdf({ path: 'test.pdf', height: '5cm', width: '10cm', printBackground: true });
+    await page.goto('http://localhost:5501');
+    await page.evaluate(() => {
+      let rows = document.querySelectorAll('tr');
+      for(let row of rows) {
+        let cells = row.querySelectorAll('td');
+        for(let cell of cells)
+          cell.innerText = '1';
+      }
+    });
+    await page.pdf({ path: './output/test.pdf', height: '5cm', width: '10cm', printBackground: true });
+    await removeLastPage('./output/test.pdf');
     await browser.close();
   })();
